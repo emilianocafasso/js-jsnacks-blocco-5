e083b1eec1af24bf73818134cff340f7ef29a4d9
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-10-22 15:36:11 +0200

    snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,6 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const student_class = students.find(student => student.name === "Marco Lanci").class
+console.log(student_class);
