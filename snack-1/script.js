1436db9d8521e7b243ea89cb51ead34529fd3f3a
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-10-22 15:20:48 +0200

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,8 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+names.forEach(name => {
+    console.log(name);
+});
+
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
