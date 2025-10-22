2a52ca138e4c97af2f4c5d2ab3b6fe93b72e7366
emilianocafasso <emiliano2.cafasso2@gmail.com>
2025-10-22 15:30:45 +0200

    snack 6

--- snack-6/script.js
+++ snack-6/script.js
@@ -12,3 +12,8 @@ const zucchine = [
 ];
 
 // A partire dall'array fornito, crea due array. Uno con le zucchine più lunghe di almeno 15cm. L'altro con le restanti.
+
+const long_zucchine = zucchine.filter(zucchina => zucchina.length >= 15)
+const short_zucchine = zucchine.filter(zucchina => zucchina.length < 15)
+console.log(long_zucchine, short_zucchine);
+
